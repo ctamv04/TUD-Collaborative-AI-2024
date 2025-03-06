0896cee72c901da3d1ce567521b6e985f5898b18
tom <tom.jacobs@momomedical.com>
2025-03-06 19:06:05 +0100

    fix test

--- agents1/OfficialAgent.py
+++ agents1/OfficialAgent.py
@@ -1172,7 +1172,7 @@ class BaselineAgent(ArtificialBrain):
                     willingness = float(row[3])
                     confidence = int(row[4])
                     trustBeliefs.setdefault(self._human_name,{}).update({task: {'competence': competence, 'willingness': willingness, 'confidence': confidence}})
-                    trustBeliefs[name] = {'competence': competence, 'willingness': willingness}
+                    trustBeliefs[name][task] = {'competence': competence, 'willingness': willingness}
                 # Initialize default trust values
                 if row and row[0] != self._human_name:
                     competence = default
